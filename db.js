a03c4d0e1152869a416fcd9086617eb7d1651758
unknown <lordssly@gmail.com>
2022-05-16 23:43:52 +0200

    ajout de la fonction Nouveau IMC

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-import {Low as low, JSONFileSync as FileSync} from 'lowdb';
+import { Low as low, JSONFileSync as FileSync } from 'lowdb';
 import lodash from 'lodash'
 
 
 const adapter = new FileSync('db.json');
 const db = new low(adapter);
-db.data={annonce:[]}
+db.data = { annonce: [] }
 await db.read()
 db.chain = lodash.chain(db.data)
 
@@ -16,55 +16,52 @@ db.chain = lodash.chain(db.data)
 
 
 
-export const listAnnonce=()=>{
-    if(db.chain==undefined){
-    db.write() }
-      let dbAnnonce = db.chain.get("annonce").value() 
-    
-    return dbAnnonce
-
+export const listAnnonce = () => {
+  if (db.chain == undefined) {
+    db.write()
   }
+  let dbAnnonce = db.chain.get("annonce").value()
+
+  return dbAnnonce
+
+}
+//--------------------------------------------Nouveau compte---------------------------------------------------------------
 
 //ajoute un nouvel utilisateur si il créer son compte
-  export const NouveauCompte=(info)=>{
+export const NouveauCompte = (info) => {
 
-    
+  //transforme l'info en object
+  let nouveauMembre = Object.values(info)
 
-    let nouveauMembre = Object.values(info)
-    let formulaireIncomplé =false
-    
-
-    //si le formulaire est complété en entier (donc different de true dans la veraible formulaireIncomplé)
-if(formulaireIncomplé!=true){
 
   // crééer un id qui sera +1 a l'id de la bd la plus haute en plusieur etape:
 
 
- // d'abord verifie le dernier objet dans la bd 
-  let longueur =db.chain.get("annonce").value().length-1
+  // d'abord verifie le dernier objet dans la bd 
+  let longueur = db.chain.get("annonce").value().length - 1
 
   let number
 
-  if(longueur != -1){
+  if (longueur != -1) {
 
-    console.log(longueur)
 
-  //ensuite ajoute l objet dans une varible
-  let toto =db.chain.get("annonce").slice(longueur).find("id").value()
 
-  //recupere l'id dans lobjet en question 
+    //ensuite ajoute l objet dans une varible
+    let toto = db.chain.get("annonce").slice(longueur).find("id").value()
 
-  let plusGrandId = toto.id
+    //recupere l'id dans lobjet en question 
 
-  //prend l'information et dit bien que ceci est un nombre
-  number = Number( plusGrandId) +1
+    let plusGrandId = toto.id
 
-}else{
-  number = 1
-}
-  
-      // ajoute la donné recupéré la variable nouveau membre , sauf l'id qui sera un "+1" du dernier id dans la bd
-  db.data.annonce.push({ id: number, Nom: nouveauMembre[0],Mdp: nouveauMembre[1], age: nouveauMembre[2],imc:[{taille: nouveauMembre[3], poids: nouveauMembre[4]}] })
+    //prend l'information et dit bien que ceci est un nombre
+    number = Number(plusGrandId) + 1
+
+  } else {
+    number = 1
+  }
+
+  // ajoute la donné recupéré la variable nouveau membre , sauf l'id qui sera un "+1" du dernier id dans la bd
+  db.data.annonce.push({ id: number, Nom: nouveauMembre[0], Mdp: nouveauMembre[1], age: nouveauMembre[2], imc: [{ taille: nouveauMembre[3], poids: nouveauMembre[4] }] })
   // ecrit dans le script bd.json 
   db.write()
 
@@ -72,17 +69,39 @@ if(formulaireIncomplé!=true){
   let PoisNombre = Number(nouveauMembre[4])
   let TailleNombre = Number(nouveauMembre[3])
 
-  let imc=  PoisNombre/(( TailleNombre* TailleNombre)/10000)
+  let imc = PoisNombre / ((TailleNombre * TailleNombre) / 10000)
 
   //console.log(imc)
   return imc
-    }else{
+
+}
 
 
 
-
-    }
-
-  }
+//______________________________________Ajout IMC ___________________________________________________________________
 
 
+export const NouveauIMC = (id,body) => {
+
+
+// recupere id du client et information body ( taille et poids)
+
+// parse l id en integer
+let idrecup =Number(id)  
+
+// recherche l'id lié au client dans la BD 
+let ImcBd = db.chain.get("annonce").find({id: idrecup}).value()
+
+//  ajoute le tableaux recuperé dans la variable imcBd (.imc est le nom du tableaux )
+let imctable=ImcBd.imc
+//push les nouvelle info dans le tableau
+imctable.push(body)
+
+//ajoute le tableaux et ecrase l ancien dans la save du client lié a l'id recuperer 
+db.chain.get("annonce").find({id: idrecup}).imc = imctable
+
+//ecrit dans le fichié
+db.write()
+
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import {listAnnonce,NouveauCompte} from "./db.js"
+import {listAnnonce,NouveauCompte,NouveauIMC} from "./db.js"
 
 //Notre objet express
 const api = express()
@@ -22,6 +22,18 @@ api.get('/annonce', (req, res) => {
 api.post('/inscription', (req, res) => {
 
     res.json(NouveauCompte(req.body))
+    
+})
+
+//recupere l 'id du client via le params.id ( inscrit dans le lien ) et les donné dans le body . Ceci ce fait dans nouveauxImc/ {id du client}
+api.post('/NouveauImc/:id', (req, res) => {
+
+    
+//envoie le information recupere dans db.js 
+    NouveauIMC(req.params.id,req.body)
+
+    //renvoie ok au client
+    res.json("ok")
     
 })
 
